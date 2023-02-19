1622cfd1272c9a499addfce34518972f8fdc7f33
Pawan kumar <121557591+Pawanminetofine@users.noreply.github.com>
2023-02-19 22:43:46 +0530

    Update Guess_The_Number_Game.c
    this is the better way to understanding  the code and you can make it better and also do not copy any code and also i want to say that do you know about rand function

--- Guess_The_Number_Game.c
+++ Guess_The_Number_Game.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<conio.h>
 #include<time.h>
+//this game you 15 attempt to complete it so best luck of you
 int main(){
 	int number, guess,no_of_guesses=1;
-	srand(time(0));
+	srand(time(0));//function for random guess
 	number = rand() % 100 + 1;
 	printf("Guess The number name\n");
 	printf("\n\tPRESS ANY NUMBER TO PLAY THE GAME\n");
-
-	getch();
 
 	do{
 		printf("\n\nGuess the number between 1 To 100 :  ");
@@ -24,20 +22,20 @@ int main(){
 		else{
 			printf("\n\n\n You Guessed The Correct Number in %d attempts\n\t",no_of_guesses);
 				if(no_of_guesses==1){
-				printf("\n\n\n\t\t\t\t*****\n\t\t\t\t\tOutstanding!\n");
+				printf("Outstanding! and you won 15 rupee caseback\n");
 				
 			}
 			else if(no_of_guesses > 1 && no_of_guesses <= 3){
-				printf("\n\n\n\t\t\t\t****\n\t\t\t\t\tWell Done!\n");
+				printf("Well Done!\n");
 			}
 			else if(no_of_guesses > 3 && no_of_guesses <= 8){
-				printf("\n\n\n\t\t\t\t***\n\t\t\t\t\tGood!\n");
+				printf("Good!\n");
 			}
 			else if(no_of_guesses >8 && no_of_guesses <= 15){
-				printf("\n\n\n\t\t\t\t**\n\t\t\t\t\tKeep it Up\n");
+				printf("Keep it Up and you can do more better\n");
 			}
 			else{
-				printf("\n\n\n\t\t\t\t*\n\t\t\t\tBetter luck next time\n");
+				printf("Better luck next time try \n");
 			}
 		}
 		no_of_guesses++;
